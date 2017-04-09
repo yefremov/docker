7747f0dd4483774ec5a528e1cf87118b758308fa
Anton Yefremov <anton.yefremov@gmail.com>
2017-04-09 14:52:12 +0300

    Update nginx-node example

--- nginx-proxy/node/index.js
+++ nginx-proxy/node/index.js
@@ -1,10 +1,17 @@
+
+/**
+ * Module dependencies.
+ */
+
 const http = require('http');
 
+// create simple http server
 const server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello, World!\n');
 });
 
+// serve on port 3000
 server.listen(3000);
 
 console.log('Listening on port 3000');
